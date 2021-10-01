ef564910c28a5e7665bd6e2a892efe560e100ae9
brian <bthomas@tsl.texas.gov>
2021-10-01 17:01:57 -0500

    takes aggregated marc xml and returns subjects only. for aspace improt

---
+++ marcXML_subjects.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import lxml.etree as ET
+
+def subjectspace (subject):
+    if subject != None:
+        while subject.startswith(" "):
+            subject = subject[1:]
+        while subject.endswith(" "):
+            subject = subject[:-1]
+        if subject.endswith("."):
+            subject = subject[:-1]
+    return subject
+
+fileguy = input("file to process including filepath: ")
+fileguy2 = input("output file name including filepath: ")
+
+nsmap = {'marc': "http://www.loc.gov/MARC21/slim"}
+
+dom = ET.parse(fileguy)
+subjectList = []
+subjects = dom.xpath("//marc:datafield", namespaces=nsmap)
+for subject in subjects:
+    thisTag = subject.attrib['tag']
+    if thisTag == '650' or thisTag == '651' or thisTag == '655':
+        subjective = subject.text
+        subject.text = subjectspace(subjective)
+        if subject in subjectList:
+            subject.getparent().remove(subject)
+        else:
+            subjectList.append(subject)
+    else:
+        subject.getparent().remove(subject)
+subjects = dom.xpath("//marc:datafield/marc:subfield", namespaces=nsmap)
+for subject in subjects:
+    subjective = subject.text
+    subject.text = subjectspace(subjective)
+dom.write(fileguy2, pretty_print=True)
