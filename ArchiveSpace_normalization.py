f93815dd3f4dd0dd02e22b55036d35e0c1909d71
brian <bthomas@tsl.texas.gov>
2021-10-22 14:13:26 -0500

    update to archivespace importer

--- ArchiveSpace_normalization.py
+++ ArchiveSpace_normalization.py
@@ -1,13 +1,15 @@
 import lxml.etree as ET
-import sys, os
+import sys, os, re
 
 def spaceinator (content):
-    while content.startswith(" "):
-        content = content[1:]
-    while content.endswith(" "):
-        content = content[:-1]
-    while content.endswith(","):
-        content = content[:-1]
+    print(content)
+    if content != None:
+        while content.startswith(" "):
+            content = content[1:]
+        while content.endswith(" "):
+            content = content[:-1]
+        while content.endswith(","):
+            content = content[:-1]
     return content
 
 nsmap = {'ead': "urn:isbn:1-931666-22-9"}
@@ -27,6 +29,21 @@ def aspace_processor (file_name):
         titleText = title.text
         titleText = spaceinator(titleText)
         title.text = titleText
+    title_emph = dom.xpath(".//ead:unittitle/ead:emph", namespaces=nsmap)
+    for title in title_emph:
+        titleText = title.text
+        if titleText.endswith(", "):
+            titleText = titleText[:-2] + '"LadyGaga'
+            title.text = titleText
+        if titleText.endswith(","):
+            titleText = titleText[:-1] + '"LadyGaga'
+            title.text = titleText
+        if titleText.endswith(',"'):
+            titleText = titleText[:-2] + '"LadyGaga'
+            title.text = titleText
+        if titleText.endswith(', "'):
+            titleText = titleText[:-3] + '"LadyGaga'
+            title.text = titleText
     turkey = dom.find(".//ead:archdesc/ead:arrangement", namespaces=nsmap)
     myRelatives = dom.xpath(".//ead:relatedmaterial/ead:relatedmaterial", namespaces=nsmap)
     for item in myRelatives:
@@ -40,7 +57,32 @@ def aspace_processor (file_name):
             turkey.addnext(item)
         if boss != None:
             boss.getparent().remove(boss)
+    containers = dom.xpath(".//ead:container", namespaces=nsmap)
+    for container in containers:
+        type = container.attrib['type']
+        if type == "box":
+            container.attrib['type'] = "Box"
+        if type == 'folder':
+            container.attrib['type'] = "Folder"
     dom.write(filename, pretty_print=True)
+    with open(filename, "r") as r:
+        filedata = r.read()
+        filedata = filedata.replace("LadyGaga</emph>", "</emph>, ")
+        filedata = filedata.replace(",  ", ", ")
+        filedata = filedata.replace(", </unittitle>", "</unittitle>")
+        filedata = filedata.replace('"</emph>,', ',"</emph>')
+        filedata = filedata.replace("http://legacy.lib.utexas.edu/taro","https://legacy.lib.utexas.edu/taro")
+        donkeykong = re.findall(r'https://\.*lib.utexas.edu/taro/\.*/\.*/\.*.html', filedata)
+        if donkeykong:
+            for item in donkeykong:
+                item = str(item)
+                temp1 = item.split("/")[-3]
+                temp2 = item.split("/")[-2]
+                new_url = f'https://www.txarchives.org/{temp1}/finding_aids/{temp2}.xml'
+                filedata = filedata.replace(item,new_url)
+        with open(filename, "w") as w:
+            w.write(filedata)
+        w.close()
 
 print("run against a directory to get broadscale changes in place for files before they get imported into the system")
 print("or run against a discreet file")
