7a6b8076d4e441282c434adb0fd802571f6c12b0
brian <bthomas@tsl.texas.gov>
2022-02-17 17:04:15 -0600

    temporary adjustment for volumes issues

--- asnake_microfilm.py
+++ asnake_microfilm.py
@@ -16,7 +16,7 @@ this_container = {'jsonmodel_type': 'top_container',
                   'series': [],
                   'collection': [],
                   'indicator': 'turkey-002',
-                  'type': 'Microfilm',
+                  'type': 'Volume',
                   'barcode': '',
                   'repository': {'ref': '/repositories/2'},
                   'restricted': 'false',
@@ -26,102 +26,108 @@ this_container = {'jsonmodel_type': 'top_container',
 client = ASnakeClient()
 
 archival_objects = (client.get("repositories/2/archival_objects", params={'all_ids': True})).json()
-address = f'repositories/2/archival_objects/{0}'
+#print(archival_objects)
+address = f'repositories/2/archival_objects/' + "{0}"
 for archival_object in tqdm(archival_objects):
     rec_uri = address.format(archival_object)
     res_record = client.get(rec_uri).json()
-    print(res_record)
-    output = open("../../.config/JetBrains/PyCharmCE2021.3/scratches/sample.json", "w")
+    output = open("sample.json", "w")
     output.write(json.dumps(res_record))
     output.close()
     listy = []
     for instance in res_record['instances']:
         selection = False
-        print(instance)
-        if 'type_3' in instance['sub_container']:
-            print("yes, key 3 exists")
-            if instance['sub_container']['type_3'] == "Microfilm":
-                print("yes")
-                selection = True
-                exists = False
-                temp1 = "Microfilm"
-                temp2 = instance['sub_container']['indicator_3']
-                print(f"checking for microfilm {temp2} in top containers before creating it")
-                response = (client.get('repositories/2/top_containers/search?q=' + temp2).json())
-                print(response)
-                if response['response']['numFound'] > 0:
-                    for widget in response['response']['docs']:
-                        listy.append({'jsonmodel_type': 'instance', 'instance_type': 'microform',
-                                      'sub_container': {'jsonmodel_type': 'sub_container',
-                                                        'top_container': {'ref': widget['id']}}})
-                    exists = True
-                    del instance['sub_container']['type_3']
-                    del instance['sub_container']['indicator_3']
-                if exists is False:
-                    print("this microfilm doesn't exist yet, creating it")
-                    current = this_container
-                    current['indicator'] = temp2
-                    response = client.post('/repositories/2/top_containers', json=this_container)
-                    print("created top container, waiting 30 seconds for changes to take effect")
-                    time.sleep(30)
+        top_container = client.get(instance['sub_container']['top_container']['ref']).json()
+        if top_container['type'] == "Microfilm":
+            print(instance)
+            if 'type_3' in instance['sub_container']:
+                print("yes, key 3 exists")
+                if instance['sub_container']['type_3'] == "Volume":
+                    print("yes")
+                    selection = True
+                    exists = False
+                    temp1 = "Volume"
+                    temp2 = instance['sub_container']['indicator_3']
+                    print(f"checking for microfilm {temp2} in top containers before creating it")
                     response = (client.get('repositories/2/top_containers/search?q=' + temp2).json())
                     print(response)
-                    for widget in response['response']['docs']:
-                        listy.append({'jsonmodel_type': 'instance', 'instance_type': 'microform',
-                                                  'sub_container': {'jsonmodel_type': 'sub_container',
-                                                                    'top_container': {'ref': widget['id']}}})
+                    if response['response']['numFound'] > 0:
+                        for widget in response['response']['docs']:
+                            if widget['indicator'] == temp2:
+                                listy.append({'jsonmodel_type': 'instance', 'instance_type': 'Mixed Materials',
+                                              'sub_container': {'jsonmodel_type': 'sub_container',
+                                                                'top_container': {'ref': widget['id']}}})
+                                exists = True
+                                del instance['sub_container']['type_3']
+                                del instance['sub_container']['indicator_3']
+                    if exists is False:
+                        print("this microfilm doesn't exist yet, creating it")
+                        current = this_container
+                        current['indicator'] = temp2
+                        response = client.post('/repositories/2/top_containers', json=this_container)
+                        print("created top container, waiting 30 seconds for changes to take effect")
+                        time.sleep(30)
+                        response = (client.get('repositories/2/top_containers/search?q=' + temp2).json())
+                        print(response)
+                        for widget in response['response']['docs']:
+                            if widget['indicator'] == temp2:
+                                listy.append({'jsonmodel_type': 'instance', 'instance_type': 'Mixed Materials',
+                                              'sub_container': {'jsonmodel_type': 'sub_container',
+                                                                'top_container': {'ref': widget['id']}}})
+                        selection = True
+                        del instance['sub_container']['type_3']
+                        del instance['sub_container']['indicator_3']
+            else:
+                print("no third key exists, checking second keys")
+            if 'type_2' in instance['sub_container']:
+                print("yes, key 2 exists")
+                if instance['sub_container']['type_2'] == "Volume":
+                    print("yes")
                     selection = True
-                    del instance['sub_container']['type_3']
-                    del instance['sub_container']['indicator_3']
-        else:
-            print("no third key exists, checking second keys")
-        if 'type_2' in instance['sub_container']:
-            print("yes, key 2 exists")
-            if instance['sub_container']['type_2'] == "Microfilm":
-                print("yes")
-                selection = True
-                exists = False
-                temp1 = "Microfilm"
-                temp2 = instance['sub_container']['indicator_2']
-                #temp2 = "2022/008-6"
-                print(f"checking for microfilm {temp2} in top containers before creating it")
-                response = (client.get('repositories/2/top_containers/search?q=' + temp2).json())
-                print(response)
-                if response['response']['numFound'] > 0:
-                    for widget in response['response']['docs']:
-                        listy.append({'jsonmodel_type': 'instance', 'instance_type': 'microform',
-                                      'sub_container': {'jsonmodel_type': 'sub_container',
-                                                        'top_container': {'ref': widget['id']}}})
-                    exists = True
-                    del instance['sub_container']['type_2']
-                    del instance['sub_container']['indicator_2']
-                if exists is False:
-                    print("this microfilm doesn't exist yet, creating it")
-                    current = this_container
-                    current['indicator'] = temp2
-                    response = client.post('/repositories/2/top_containers', json=this_container)
-                    print("created top container, waiting 30 seconds for changes to take effect")
-                    time.sleep(30)
+                    exists = False
+                    temp1 = "Volume"
+                    temp2 = instance['sub_container']['indicator_2']
+                    #temp2 = "2022/008-6"
+                    print(f"checking for microfilm {temp2} in top containers before creating it")
                     response = (client.get('repositories/2/top_containers/search?q=' + temp2).json())
                     print(response)
-                    for widget in response['response']['docs']:
-                        listy.append({'jsonmodel_type': 'instance', 'instance_type': 'microform',
-                                                  'sub_container': {'jsonmodel_type': 'sub_container',
-                                                                    'top_container': {'ref': widget['id']}}})
-                    selection = True
-                    del instance['sub_container']['type_2']
-                    del instance['sub_container']['indicator_2']
-        else:
-            print("no third key exists, checking second keys")
-    if selection is True:
-        print(listy)
-        for item in listy:
-            res_record['instances'].append(item)
-        response = (client.post(rec_uri, json=res_record)).json()
-        print(response)
-        res_record = client.get(rec_uri).json()
-        output = open("../../.config/JetBrains/PyCharmCE2021.3/scratches/sample.json", "w")
-        output.write(json.dumps(res_record))
-        output.close()
+                    if response['response']['numFound'] > 0:
+                        for widget in response['response']['docs']:
+                            if widget['indicator'] == temp2:
+                                listy.append({'jsonmodel_type': 'instance', 'instance_type': 'Mixed Materials',
+                                              'sub_container': {'jsonmodel_type': 'sub_container',
+                                                                'top_container': {'ref': widget['id']}}})
+                                exists = True
+                                del instance['sub_container']['type_2']
+                                del instance['sub_container']['indicator_2']
+                    if exists is False:
+                        print("this microfilm doesn't exist yet, creating it")
+                        current = this_container
+                        current['indicator'] = temp2
+                        response = client.post('/repositories/2/top_containers', json=this_container)
+                        print("created top container, waiting 30 seconds for changes to take effect")
+                        time.sleep(30)
+                        response = (client.get('repositories/2/top_containers/search?q=' + temp2).json())
+                        print(response)
+                        for widget in response['response']['docs']:
+                            if widget['indicator'] == temp2:
+                                listy.append({'jsonmodel_type': 'instance', 'instance_type': 'Mixed Materials',
+                                              'sub_container': {'jsonmodel_type': 'sub_container',
+                                                                'top_container': {'ref': widget['id']}}})
+                        selection = True
+                        del instance['sub_container']['type_2']
+                        del instance['sub_container']['indicator_2']
+            else:
+                print("no third key exists, checking second keys")
+        if selection is True:
+            print(listy)
+            for item in listy:
+                res_record['instances'].append(item)
+            response = (client.post(rec_uri, json=res_record)).json()
+            print(response)
+            res_record = client.get(rec_uri).json()
+            output = open("sample.json", "w")
+            output.write(json.dumps(res_record))
+            output.close()
 end = time.asctime()
 print(f"all done! Started at {start} and ended at {end}")
