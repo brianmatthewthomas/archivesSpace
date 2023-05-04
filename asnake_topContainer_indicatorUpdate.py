cd58fe2ea1adcd7c7ccabc61f61bd65a070ac801
brian <bthomas@tsl.texas.gov>
2023-05-04 11:37:54 -0500

    update method to accomodate a 3 column spreadsheet with specific indicators due to the sheer volume of boxes

--- asnake_topContainer_indicatorUpdate.py
+++ asnake_topContainer_indicatorUpdate.py
@@ -1,6 +1,7 @@
 from asnake.client import ASnakeClient
 import asnake.logging as logging
 import datetime
+import pandas as PD
 import json
 import time
 today_date = datetime.datetime.today().strftime('%Y-%m-%d')
@@ -8,10 +9,33 @@ logging.setup_logging(filename='extent_type_changer_' + str(today_date) + '.log'
 logger = logging.get_logger('extent_type_changes_log')
 
 #logging.setup_logging(filename="../../.config/JetBrains/PyCharmCE2021.3/scratches/aris.log", filemode="a")
+def row_converter(row=tuple, listy=list, verbose=True):
+    #convert pandas row to a dictionary
+    #requires a list of columns and a row as a tuple
+    count = 1
+    pictionary = {}
+    pictionary['Index'] = row[0]
+    for item in listy:
+        pictionary[item] = row[count]
+        count += 1
+    if verbose is True:
+        print(pictionary)
+    return pictionary
 
+spreadsheet = "/media/sf_Documents/top_container_changes.xlsx"
+df = PD.read_excel(spreadsheet, dtype=object)
+listy = df.columns
+
+title_dict = {}
+for row in df.itertuples():
+    valuables = row_converter(row, listy, verbose=False)
+    title_dict[valuables['Old Top Container Indicator']] = [valuables['New top container indicator'], valuables['identifier']]
+print(title_dict)
+start_up = input("press enter to continue")
 client = ASnakeClient()
-responses = (client.get('repositories/2/top_containers/search?q=223-').json())
+responses = (client.get('repositories/2/top_containers?all_ids=True').json())
 print(responses)
+'''
 responses2 = responses['response']['docs']
 for response in responses2:
     if "223-" in response['title']:
@@ -30,8 +54,18 @@ for response in responses2:
             print(f"update status: {submission}")
 '''
 counter = 0
-for response in responses:
-    container = client.get(response['ref']).json()
+for key in title_dict.keys():
+    ref = title_dict[key][-1]
+    container = client.get(f"repositories/2/top_containers/{ref}").json()
+    if container['indicator'] in title_dict.keys():
+        print(container)
+        old = container['indicator']
+        new_indicator = title_dict[container['indicator']][0]
+        print(container['indicator'])
+        print(new_indicator)
+        container['indicator'] = new_indicator
+        submission = client.post(container['uri'], json=container).json()
+    '''
     if "type" in container:
         if container['type'] == "Reel":
             container['type'] = "Microfilm"
@@ -45,4 +79,4 @@ for response in responses:
     output.write(json.dumps(response))
     output.close()
 print(counter,"items updated")
-'''
+    '''
